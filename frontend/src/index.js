f967a0e0a0caee2ba2baf786bd949b9dca8a048b
najmudheen m <najmudheen.kottakuth@gmail.com>
2022-03-04 16:19:48 +0530

    ...

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,11 +13,11 @@ import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <PersistGate persistor={persistor}>
           <App />
         </PersistGate>
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
